f3666068394fbe9cd05cd9d30bb2224eabd743b5
Humberto Ramírez <humberto.rjdh@gmail.com>
2020-11-04 23:00:39 -0600

    Initialize map

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +7,13 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'arcgis-comitan-map';
+  // Set our map properties
+  mapCenter = [-92.133333, 16.25];
+  basemapType = 'gray-vector';
+  mapZoomLevel = 12;
+
+  mapLoadedEvent(status: boolean): void {
+    console.log('the map loaded: ' + status);
+  }
+
 }
