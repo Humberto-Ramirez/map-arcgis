39ca018d3886412139500371c1ca54d6f5bbd0a5
Humberto Ramírez <humberto.rjdh@gmail.com>
2020-11-05 18:08:10 -0600

    Add urban shapes

---
+++ src/app/esri-map/shapes/urban_shapes.ts
@@ -0,0 +1,183 @@
+////////////////////////////////////////////////
+// URBAN SHAPES
+////////////////////////////////////////////////
+
+import {
+  secc239Shape,
+  secc240Shape,
+  secc241Shape,
+  secc242Shape,
+  secc243Shape,
+  secc244Shape,
+  secc245Shape,
+  secc246Shape,
+  secc247Shape,
+  secc248Shape,
+  secc249Shape,
+  secc250Shape,
+  secc251Shape,
+  secc252Shape,
+  secc253Shape,
+  secc254Shape,
+  secc255Shape,
+  secc256Shape,
+  secc257Shape,
+  secc258Shape,
+  secc259Shape,
+  secc260Shape,
+  secc261Shape,
+  secc262Shape,
+  secc263Shape,
+  secc264Shape,
+  secc265Shape,
+  secc266Shape,
+  secc267Shape
+} from './urban';
+
+const ss1 = {
+  type: 'polyline',
+  paths: secc239Shape
+};
+const ss2 = {
+  type: 'polyline',
+  paths: secc240Shape
+};
+const ss3 = {
+  type: 'polyline',
+  paths: secc241Shape
+};
+const ss4 = {
+  type: 'polyline',
+  paths: secc242Shape
+};
+const ss5 = {
+  type: 'polyline',
+  paths: secc243Shape
+};
+const ss6 = {
+  type: 'polyline',
+  paths: secc244Shape
+};
+const ss7 = {
+  type: 'polyline',
+  paths: secc245Shape
+};
+const ss8 = {
+  type: 'polyline',
+  paths: secc246Shape
+};
+const ss9 = {
+  type: 'polyline',
+  paths: secc247Shape
+};
+const ss10 = {
+  type: 'polyline',
+  paths: secc248Shape
+};
+const ss11 = {
+  type: 'polyline',
+  paths: secc249Shape
+};
+const ss12 = {
+  type: 'polyline',
+  paths: secc250Shape
+};
+const ss13 = {
+  type: 'polyline',
+  paths: secc251Shape
+};
+const ss14 = {
+  type: 'polyline',
+  paths: secc252Shape
+};
+const ss15 = {
+  type: 'polyline',
+  paths: secc253Shape
+};
+const ss16 = {
+  type: 'polyline',
+  paths: secc254Shape
+};
+const ss17 = {
+  type: 'polyline',
+  paths: secc255Shape
+};
+const ss18 = {
+  type: 'polyline',
+  paths: secc256Shape
+};
+const ss19 = {
+  type: 'polyline',
+  paths: secc257Shape
+};
+const ss20 = {
+  type: 'polyline',
+  paths: secc258Shape
+};
+const ss21 = {
+  type: 'polyline',
+  paths: secc259Shape
+};
+const ss22 = {
+  type: 'polyline',
+  paths: secc260Shape
+};
+const ss23 = {
+  type: 'polyline',
+  paths: secc261Shape
+};
+const ss24 = {
+  type: 'polyline',
+  paths: secc262Shape
+};
+const ss25 = {
+  type: 'polyline',
+  paths: secc263Shape
+};
+const ss26 = {
+  type: 'polyline',
+  paths: secc264Shape
+};
+const ss27 = {
+  type: 'polyline',
+  paths: secc265Shape
+};
+const ss28 = {
+  type: 'polyline',
+  paths: secc266Shape
+};
+const ss29 = {
+  type: 'polyline',
+  paths: secc267Shape
+};
+
+export const urbanShapes = [ss1,
+  ss2,
+  ss3,
+  ss4,
+  ss5,
+  ss6,
+  ss7,
+  ss8,
+  ss9,
+  ss10,
+  ss11,
+  ss12,
+  ss13,
+  ss14,
+  ss15,
+  ss16,
+  ss17,
+  ss18,
+  ss19,
+  ss20,
+  ss21,
+  ss22,
+  ss23,
+  ss24,
+  ss25,
+  ss26,
+  ss27,
+  ss28,
+  ss29
+];
