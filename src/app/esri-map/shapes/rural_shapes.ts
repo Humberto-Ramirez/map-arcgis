4c2c93ca45cb38d1eb0697b6cd9af63e5883ddcb
Humberto Ramírez <humberto.rjdh@gmail.com>
2020-11-05 18:37:09 -0600

    Add rural shapes

---
+++ src/app/esri-map/shapes/rural_shapes.ts
@@ -0,0 +1,80 @@
+import {
+  secc279Shape,
+  secc282Shape,
+  secc283Shape,
+  secc284Shape,
+  secc285Shape,
+  secc286Shape,
+  secc287Shape,
+  secc288Shape,
+  secc289Shape, secc290Shape, secc291Shape
+} from './rural';
+
+const ss1 = {
+  type: 'polyline',
+  paths: secc279Shape
+};
+
+const ss2 = {
+  type: 'polyline',
+  paths: secc282Shape
+};
+
+const ss3 = {
+  type: 'polyline',
+  paths: secc283Shape
+};
+
+const ss4 = {
+  type: 'polyline',
+  paths: secc284Shape
+};
+
+const ss5 = {
+  type: 'polyline',
+  paths: secc285Shape
+};
+
+const ss6 = {
+  type: 'polyline',
+  paths: secc286Shape
+};
+
+const ss7 = {
+  type: 'polyline',
+  paths: secc287Shape
+};
+
+const ss8 = {
+  type: 'polyline',
+  paths: secc288Shape
+};
+
+const ss9 = {
+  type: 'polyline',
+  paths: secc289Shape
+};
+
+const ss10 = {
+  type: 'polyline',
+  paths: secc290Shape
+};
+
+const ss11 = {
+  type: 'polyline',
+  paths: secc291Shape
+};
+
+export const ruralShapes = [
+  ss1,
+  ss2,
+  ss3,
+  ss4,
+  ss5,
+  ss6,
+  ss7,
+  ss8,
+  ss9,
+  ss10,
+  ss11
+];
