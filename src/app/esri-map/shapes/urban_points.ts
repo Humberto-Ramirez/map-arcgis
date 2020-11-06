eada96438db198c1a91130c5e381d3255d37be9b
Humberto Ramírez <humberto.rjdh@gmail.com>
2020-11-05 20:04:42 -0600

    Add urban points

---
+++ src/app/esri-map/shapes/urban_points.ts
@@ -0,0 +1,180 @@
+const secc239Point = {
+  name: 'secc_239',
+  point: [-92.1524, 16.2519]
+};
+
+const secc240Point = {
+  name: 'secc_240',
+  point: [-92.1445, 16.2562]
+};
+
+const secc241Point = {
+  name: 'secc_241',
+  point: [-92.1404, 16.2557]
+};
+
+const secc242Point = {
+  name: 'secc_242',
+  point: [-92.1407, 16.2589]
+};
+
+const secc243Point = {
+  name: 'secc_243',
+  point: [-92.1336, 16.2594]
+};
+
+const secc244Point = {
+  name: 'secc_244',
+  point: [-92.1295, 16.2633]
+};
+
+const secc245Point = {
+  name: 'secc_245',
+  point: [-92.1313, 16.2564]
+};
+
+const secc246Point = {
+  name: 'secc_246',
+  point: [-92.1359, 16.2557]
+};
+
+const secc247Point = {
+  name: 'secc_247',
+  point: [-92.1396, 16.2524]
+};
+
+const secc248Point = {
+  name: 'secc_248',
+  point: [-92.1514, 16.2476]
+};
+
+const secc249Point = {
+  name: 'secc_249',
+  point: [-92.1497, 16.2427]
+};
+
+const secc250Point = {
+  name: 'secc_250',
+  point: [-92.1410, 16.2456]
+};
+
+const secc251Point = {
+  name: 'secc_251',
+  point: [-92.1365, 16.2492]
+};
+
+const secc252Point = {
+  name: 'secc_252',
+  point: [-92.1341, 16.2532]
+};
+
+const secc253Point = {
+  name: 'secc_253',
+  point: [-92.1324, 16.2508]
+};
+
+const secc254Point = {
+  name: 'secc_254',
+  point: [-92.1293, 16.2528]
+};
+
+const secc255Point = {
+  name: 'secc_255',
+  point: [-92.1187, 16.2564]
+};
+
+const secc256Point = {
+  name: 'secc_256',
+  point: [-92.1115, 16.2532]
+};
+
+const secc257Point = {
+  name: 'secc_257',
+  point: [-92.1254, 16.2477]
+};
+
+const secc258Point = {
+  name: 'secc_258',
+  point: [-92.1302, 16.2475]
+};
+
+const secc259Point = {
+  name: 'secc_259',
+  point: [-92.1338, 16.2430]
+};
+
+const secc260Point = {
+  name: 'secc_260',
+  point: [-92.1502, 16.2368]
+};
+
+const secc261Point = {
+  name: 'secc_261',
+  point: [-92.1402, 16.2329]
+};
+
+const secc262Point = {
+  name: 'secc_262',
+  point: [-92.1288, 16.2366]
+};
+
+const secc263Point = {
+  name: 'secc_263',
+  point: [-92.1219, 16.2423]
+};
+
+const secc264Point = {
+  name: 'secc_264',
+  point: [-92.1106, 16.2313]
+};
+
+const secc265Point = {
+  name: 'secc_265',
+  point: [-92.1246, 16.2322]
+};
+
+const secc266Point = {
+  name: 'secc_266',
+  point: [-92.1294, 16.2153]
+};
+
+const secc267Point = {
+  name: 'secc_267',
+  point: [-92.1197, 16.2271]
+};
+
+export const urbanPoints = [
+  secc239Point,
+  secc240Point,
+  secc241Point,
+  secc242Point,
+  secc243Point,
+  secc244Point,
+  secc245Point,
+  secc246Point,
+  secc247Point,
+  secc248Point,
+  secc249Point,
+  secc250Point,
+  secc251Point,
+  secc252Point,
+  secc253Point,
+  secc254Point,
+  secc255Point,
+  secc256Point,
+  secc257Point,
+  secc258Point,
+  secc259Point,
+  secc260Point,
+  secc261Point,
+  secc262Point,
+  secc263Point,
+  secc264Point,
+  secc265Point,
+  secc266Point,
+  secc267Point
+];
+
+
+
+
