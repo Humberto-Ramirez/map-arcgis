2071f77fadac9ff83b0e75dcfefd10fff2b153b6
Humberto Ramírez <humberto.rjdh@gmail.com>
2020-11-05 20:14:24 -0600

    Add rural points

---
+++ src/app/esri-map/shapes/rural_points.ts
@@ -0,0 +1,68 @@
+const secc279Point = {
+  name: 'secc_279',
+  point: [-92.0744, 16.3468]
+};
+
+const secc282Point = {
+  name: 'secc_282',
+  point: [-92.1175, 16.2929]
+};
+
+const secc283Point = {
+  name: 'secc_283',
+  point: [-92.0836, 16.3020]
+};
+
+const secc284Point = {
+  name: 'secc_284',
+  point: [-92.0704, 16.2765]
+};
+
+const secc285Point = {
+  name: 'secc_285',
+  point: [-92.0520, 16.2604]
+};
+
+const secc286Point = {
+  name: 'secc_286',
+  point: [-92.0887, 16.2518]
+};
+
+const secc287Point = {
+  name: 'secc_287',
+  point: [-92.0623, 16.2221]
+};
+
+const secc288Point = {
+  name: 'secc_288',
+  point: [-92.0421, 16.1809]
+};
+
+const secc289Point = {
+  name: 'secc_289',
+  point: [-92.1056, 16.1873]
+};
+
+const secc290Point = {
+  name: 'secc_290',
+  point: [-92.1168, 16.1433]
+};
+
+const secc291Point = {
+  name: 'secc_291',
+  point: [-92.1756, 16.0788]
+};
+
+export const ruralPoints = [
+  secc279Point,
+  secc282Point,
+  secc283Point,
+  secc284Point,
+  secc285Point,
+  secc286Point,
+  secc287Point,
+  secc288Point,
+  secc289Point,
+  secc290Point,
+  secc291Point
+];
